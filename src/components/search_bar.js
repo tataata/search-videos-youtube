083f6dfe76e57ef6cf57e6bcded517248e41ecce
tataata <fortataata@gmail.com>
2017-09-14 20:39:44 +0200

    create new status for selected videos; styling searchbar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,9 +10,8 @@ class SearchBar extends Component {
 
 	render() {
 		return (
-			<div>
-        
-				<input 
+			<div className="search-bar">
+				<input
           value = {this.state.term}
           onChange={event => this.setState({ term: event.target.value })} />
 			</div>
--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ class App extends Component {
 
     this.state = {
       videos: [],
-      selectedVideo: null,
+      selectedVideo: null
     };
 
     YTSearch({key: API_KEY, term: 'cats'}, (videos) => {
